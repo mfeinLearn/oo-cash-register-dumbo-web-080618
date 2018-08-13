be85373b954732bd9de7318bf2b23f24f0921fb7
Malcome Fein <mfein90@gmail.com>
2018-08-13 07:58:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
+ attr_accessor :total, :discount
+  def initialize(total = 0)
+    @total = 0
+    @discount = total - (total*0.20)
 
+  end 
+  
+  def total
+    @total
+  end
+  
+end
